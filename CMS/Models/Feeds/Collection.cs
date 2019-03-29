66c4afb2e8bfb432fcfa0ff9fbb1d1f11e0ccb1f
Olex <entropy.alex525@gmail.com>
2019-03-29 19:11:29 +0200

    Add  test program - console app

--- CMS/Models/Feeds/Collection.cs
+++ CMS/Models/Feeds/Collection.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace CMS.Models.Feeds
@@ -9,7 +11,9 @@ namespace CMS.Models.Feeds
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        
+
+        [JsonIgnore]
+        [IgnoreDataMember]
         public IList<CollectionFeed> CollectionFeed { get; set; }
 
     }
--- TestConsoleAppCMS/Program.cs
+++ TestConsoleAppCMS/Program.cs
@@ -1,9 +1,7 @@
 ﻿using RestSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+
 
 namespace TestConsoleAppCMS
 {
@@ -11,25 +9,75 @@ namespace TestConsoleAppCMS
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("http://localhost:50563/");
+            var client = new RestClient("http://localhost:57578/api/");
 
-
-            // post
-            var request = new RestRequest("api/collection", Method.POST);
-            request.AddParameter("Name", "New collection 1"); // adds to POST or URL querystring based on Method
-            //request.AddUrlSegment("id", "123"); // replaces matching token in request.Resource
+            // api/collection get
+            Console.WriteLine("We make GER  request to localhost:57578/api/collection");
+            var request = new RestRequest("collection", Method.GET);
             IRestResponse response = client.Execute(request);
             var content = response.Content;
-            Console.WriteLine("We create new collection. Id = " + content);
+            Console.WriteLine("We find collections " + content);
 
-            // put RSS
-            //var request1 = new RestRequest("api/collection", Method.PUT);
-            //request1.AddParameter("Url", "https://www.feedforall.com/blog-feed.xml");
-            //request1.AddParameter("FeedType", "RSS");
-            //IRestResponse response1 = client.Execute(request1);
-            //content = response.Content;
-            //Console.WriteLine("We create new collection. Id = " + content);
 
+
+            // api/post  post
+            Console.WriteLine("We try POST  request to localhost:57578/api/collection");
+            var requestPost = new RestRequest("collection", Method.POST);
+            requestPost.RequestFormat = DataFormat.Json;
+            Console.Write("Input new collection name: ");
+            var name = Console.ReadLine();
+            // create new collection item
+            requestPost.AddJsonBody(
+               new 
+               {
+                   Name = name
+               });
+
+
+            var responsePost = client.Execute(requestPost);
+
+            var contentPost = responsePost.Content;
+            int collectionId = int.Parse(contentPost);
+            if (responsePost.StatusCode == HttpStatusCode.OK)
+            {
+               
+                
+                Console.WriteLine("We create new collection. Id = " + collectionId);
+            }
+            else
+            {
+                Console.WriteLine("Something wrong... ");
+            }
+
+            // PUT api/Collection/5  Add feed to a collection
+            Console.WriteLine("We try PUT  request to localhost:57578/api/collection/" + collectionId);
+            var requestPut = new RestRequest("collection/" + collectionId,  Method.PUT);
+            requestPut.RequestFormat = DataFormat.Json;
+            Console.Write("Input url, that you want to add to collection "+ name+" :");
+            var url = Console.ReadLine();
+            Console.Write("Input feed type ( 1- RSS, 2- Atom) :");
+            var type = Console.ReadLine();
+            // create new feed item
+            requestPut.AddJsonBody(
+               new
+               {
+                   Url = url,
+                   FeedType = type
+               });
+            var responsetPut = client.Execute(requestPut);
+            var contentPut = responsetPut.Content;
+            Console.WriteLine("Put operation ctatus : " + contentPut);
+
+
+
+            // api/collection/id get
+            Console.WriteLine("We make GER  request to localhost:57578/api/collection/" + collectionId);
+            var client1 = new RestClient("http://localhost:57578/api/");
+            var request1 = new RestRequest("collection/" + collectionId, Method.GET);
+            IRestResponse response1 = client.Execute(request1);
+            var content1 = response1.Content;
+            Console.WriteLine("We find news in new collection where must be news from new feed ");
+            Console.WriteLine(content1);
             Console.ReadKey();
         }
     }
